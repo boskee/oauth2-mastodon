8d854dcfcbfdf239875d02019d105d77df7be38f
Wojciech Zylinski <w.zylinski@boskee.co.uk>
2022-11-12 13:39:36 +0000

    Added more properties to Resource Owner

--- src/Provider/MastodonResourceOwner.php
+++ src/Provider/MastodonResourceOwner.php
@@ -13,7 +13,6 @@ use League\OAuth2\Client\Tool\ArrayAccessorTrait;
 
 class MastodonResourceOwner implements ResourceOwnerInterface
 {
-
     use ArrayAccessorTrait;
 
     /**
@@ -42,6 +41,101 @@ class MastodonResourceOwner implements ResourceOwnerInterface
         return $this->getValueByKey($this->response, 'username');
     }
 
+    /**
+     * @return string
+     */
+    public function getAcct()
+    {
+        return $this->getValueByKey($this->response, 'acct');
+    }
+
+    /**
+     * @return string
+     */
+    public function getDisplayName()
+    {
+        return $this->getValueByKey($this->response, 'display_name');
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->getValueByKey($this->response, 'url');
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvatar()
+    {
+        return $this->getValueByKey($this->response, 'avatar');
+    }
+
+    /**
+     * @return string
+     */
+    public function getAvatarStatic()
+    {
+        return $this->getValueByKey($this->response, 'avatar_static');
+    }
+
+    /**
+     * @return string
+     */
+    public function getHeader()
+    {
+        return $this->getValueByKey($this->response, 'header');
+    }
+
+    /**
+     * @return string
+     */
+    public function getHeaderStatic()
+    {
+        return $this->getValueByKey($this->response, 'header_static');
+    }
+
+    /**
+     * @return string
+     */
+    public function getNote()
+    {
+        return $this->getValueByKey($this->response, 'note');
+    }
+
+    /**
+     * @return integer
+     */
+    public function getFollowersCount()
+    {
+        return $this->getValueByKey($this->response, 'followers_count');
+    }
+
+    /**
+     * @return integer
+     */
+    public function getFollowingCount()
+    {
+        return $this->getValueByKey($this->response, 'following_count');
+    }
+
+    /**
+     * @return integer
+     */
+    public function getStatusesCount()
+    {
+        return $this->getValueByKey($this->response, 'statuses_count');
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->getValueByKey($this->response, 'created_at');
+    }
 
     /**
      * @return array
